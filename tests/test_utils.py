75503f6d3527937935266a9dd8d419af36583847
Srinivas Gorur-Shandilya <code@srinivas.gs>
2024-12-02 13:51:38 -0500

    green ci

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,4 +7,4 @@ from thagomizer.utils import hash_file
 
 def test_hash_file():
     path = Path(__file__).parent.parent / "src" / "thagomizer" / "utils.py"
-    assert hash_file(path) == "42de5123"
+    assert hash_file(path) == "34173abd"
