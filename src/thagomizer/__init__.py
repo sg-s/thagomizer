62ba407f65c60fcfa941774c94d6c46d4e8fbe16
Srinivas Gorur-Shandilya <code@srinivas.gs>
2025-02-25 13:02:37 -0500

    feat: version 0.3.0

--- src/thagomizer/__init__.py
+++ src/thagomizer/__init__.py
@@ -1,3 +1,11 @@
 """thagomizer: useful tools in python."""
 
-from thagomizer.utils import *
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # for Python < 3.8
+    from importlib_metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("thagomizer")
+except PackageNotFoundError:
+    __version__ = "0.2.0"
